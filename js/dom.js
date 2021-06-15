4b1bbbb542be7523debb9db19e628c2a36810930
wahudamon <afwa.bagas.1999@gmail.com>
2021-06-15 12:20:55 +0700

    feat: add function to make todo and show it

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,27 @@
+const UNCOMPLETED_LIST_TODO_ID = "todos";
+
 function addTodo() {
+  const uncompletedTODOList = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
   const textTodo = document.getElementById("title").value;
   const timestamp = document.getElementById("date").value;
-  console.log("todo " + textTodo);
-  console.log("timestamp " + timestamp);
+
+  for(let i = 0; i < 10; i++) {
+    const todo = makeTodo(textTodo, timestamp);
+    uncompletedTODOList.append(todo);
+  }
+}
+
+function makeTodo(data, timestamp) {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = data;
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = timestamp;
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+
+  return container;
 }
