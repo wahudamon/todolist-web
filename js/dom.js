88942298a77dd58ba93683f9e31bf4ceb37225e8
wahudamon <afwa.bagas.1999@gmail.com>
2021-06-15 11:36:00 +0700

    feat: make dom.js script and attach it on index.html

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+function addTodo() {
+  const textTodo = document.getElementById("title").value;
+  const timestamp = document.getElementById("date").value;
+  console.log("todo " + textTodo);
+  console.log("timestamp " + timestamp);
+}
