5ad4b5d0f9b0b13f65e05bc1e9b6484efddf18fd
wahudamon <afwa.bagas.1999@gmail.com>
2021-06-25 09:27:16 +0700

    feat: call refreshDataFromTodos function in script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
     event.preventDefault();
     addTodo();
   });
+
+  if(isStorageExist()) {
+    loadDataFromStorage();
+  }
+});
+
+document.addEventListener("ondatasaved", () => {
+  console.log("data berhasil disimpan.");
+});
+
+document.addEventListener("ondataloaded", () => {
+  refreshDataFromTodos();
 });
